f92447f3a0381ecf2b30e7ee91de4630785cc5f3
AdityaSingh-web18 <adityasingh180710@gmail.com>
2024-08-24 23:11:07 +0530

    1st update

---
+++ src/components/Quiz.jsx
@@ -0,0 +1,17 @@
+
+
+
+function Quiz() {
+
+  return (
+    <div id="quiz">
+      <div id="question">
+        <h2>
+          {}
+        </h2>
+      </div>
+    </div>
+  );
+}
+
+export default Quiz;
